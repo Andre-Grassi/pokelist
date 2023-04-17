635219ab3050c95ec26485f93369e49d965d747c
Andre-Grassi <andregrassi.j@gmail.com>
2023-04-17 02:21:49 -0300

    Remove unecessary import in fetchPokemon.ts

--- src/components/helpers/fetchPokemon.ts
+++ src/components/helpers/fetchPokemon.ts
@@ -1,5 +1,3 @@
-import ReactDOM from 'react'
-
 async function fetchPokemon(pokemonName: string = '') {
   let pokemon: any
   // será que da pra colocar um return dentro do último then e fazer return const pokemon = await fetch(...) [acho que daí nem precisa do await]
