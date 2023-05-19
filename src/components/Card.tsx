99d6fde2b443b82fd0888b3ea77f232c47da585f
Andre-Grassi <andregrassi.j@gmail.com>
2023-05-19 01:27:08 -0300

    Centralize and improve styling of Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,13 +5,17 @@ const Card: React.FC<{
   alt: string
 }> = props => {
   return (
-    <div className='card'>
+    <div className='card container-sm'>
       {props.img && (
-        <img src={props.img} alt={props.alt} className='card-img-top' />
+        <img
+          src={props.img}
+          alt={props.alt}
+          className='card-img-top w-25 mx-auto border-bottom'
+        />
       )}
-      <div className='card-body'>
-        <div className='card-title'>{props.title}</div>
-        <p className='card-text'>{props.children}</p>
+      <div className='card-body mx-5'>
+        <div className='card-title text-center'>{props.title}</div>
+        <p className='card-text text-start'>{props.children}</p>
       </div>
     </div>
   )
