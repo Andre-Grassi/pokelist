d62a8eafe4c224dc92077e8c798c2b2232bf9e45
Andre-Grassi <andregrassi.j@gmail.com>
2023-05-19 01:14:07 -0300

    Fix the error in fetchPokemon (simplifying it caused an error)

--- src/components/helpers/fetchPokemon.ts
+++ src/components/helpers/fetchPokemon.ts
@@ -1,13 +1,16 @@
 async function fetchPokemon(pokemonName: string = '') {
+  let pokemon: any
   // será que da pra colocar um return dentro do último then e fazer return const pokemon = await fetch(...) [acho que daí nem precisa do await]
   await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then(response => {
       return response.json()
     })
     .then(pokemonObj => {
-      console.log(pokemonObj)
-      return pokemonObj
+      pokemon = pokemonObj
+      console.log(pokemon)
     })
+
+  return pokemon
 }
 
 export default fetchPokemon
