dba9359da096b37b1135e113ccace2a92e1ff09a
Andre-Grassi <andregrassi.j@gmail.com>
2023-05-19 01:09:59 -0300

    Simplify fetchPokemon funcion

--- src/components/helpers/fetchPokemon.ts
+++ src/components/helpers/fetchPokemon.ts
@@ -1,16 +1,13 @@
 async function fetchPokemon(pokemonName: string = '') {
-  let pokemon: any
   // será que da pra colocar um return dentro do último then e fazer return const pokemon = await fetch(...) [acho que daí nem precisa do await]
   await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then(response => {
       return response.json()
     })
     .then(pokemonObj => {
-      pokemon = pokemonObj
-      console.log(pokemon)
+      console.log(pokemonObj)
+      return pokemonObj
     })
-
-  return pokemon
 }
 
 export default fetchPokemon
