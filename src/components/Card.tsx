67502287a74b76d703cf57d8f7f698587697063b
Andre-Grassi <andregrassi.j@gmail.com>
2023-05-19 02:46:31 -0300

    Style card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,23 +10,23 @@ const Card: React.FC<{
   let pokemonStats = JSON.parse(props.children as string)
 
   return (
-    <div className='card container-sm'>
+    <div className='card container-sm col-12 col-sm-6 col-lg-4'>
       {props.img && (
         <img
           src={props.img}
           alt={props.alt}
-          className='card-img-top w-25 mx-auto border-bottom'
+          className='card-img-top w-50 mx-auto border-bottom'
         />
       )}
       <div className='card-body mx-5'>
-        <div className='card-title text-center'>
+        <div className='card-title text-center fw-bold'>
           {capitalizeFirstLetter(props.title)}
         </div>
         <div className='card-text text-start'>
-          <ul>
-            <li>HP: {pokemonStats.hp}</li>
-            <li>Attack: {pokemonStats.attack}</li>
-            <li>Defense: {pokemonStats.defense}</li>
+          <ul className='list-group '>
+            <li className='list-group-item'>HP: {pokemonStats.hp}</li>
+            <li className='list-group-item'>Attack: {pokemonStats.attack}</li>
+            <li className='list-group-item'>Defense: {pokemonStats.defense}</li>
           </ul>
         </div>
       </div>
